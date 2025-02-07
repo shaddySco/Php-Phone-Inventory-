25340f599e880f81f19c56717d631a14ce29f923
shaddySco <scowahinya@gmail.com>
2025-02-07 13:02:58 +0300

    Initial commit - Uploading project

---
+++ create.php
@@ -0,0 +1,96 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Phones', 'root' , '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+$errors = [];
+
+$title = '';
+$color = '';
+$price = '';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+$title = $_POST['Title'];
+$color = $_POST['Color'];
+$price = $_POST['Price'];
+$date = date('Y-m-d H:i:s');
+
+
+
+ //form validation
+ if(!$title)
+ {
+    $errors[] = 'Product title is required';
+ }
+
+ if(!$price){
+    $errors[] = 'Product price is required';
+ }
+
+ if (empty($errors)){
+
+ 
+ 
+$statement = $pdo->prepare("INSERT INTO Phones (Title, Image, Color, Price, Create_date)
+            VALUES(:title, :image, :color, :price, :date)");
+
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':image', '');
+            $statement->bindValue(':color', $description);
+            $statement->bindValue(':price', $price);
+            $statement->bindValue(':date', $date);
+            $statement->execute();
+}
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="app.css">
+    <title>Product CRUD</title>
+  </head>
+  <body>
+    <h1>Create new products</h1>
+
+    <?php if (!empty($errors)): ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error): ?>
+                <div><?php echo $error ?></div>
+                <?php endforeach; ?>
+        </div>
+        <?php endif;?>
+    <form action="" method="post">
+  <div class="form-group">
+    <label>Product Image</label>
+    <input type="file" name="Image" >
+  </div>
+  <div class="form-group">
+    <label>Product Title</label>
+    <input type="text" name="Title" class="form-control" value ="<?php echo $title?>">
+  </div>
+  <div class="form-group">
+    <label>Product Color</label>
+    <textarea class="form-control" name="color"></textarea>
+  </div>
+  
+  <div class="form-group">
+    <label>Product Price</label>
+    <input type="number" step=".01" name="Price" class="form-control" >
+  </div>
+  
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+   
+   
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root' , '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT*FROM Products ORDER BY create_date DESC');
+$statement -> execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="app.css">
+    <title>Product CRUD</title>
+  </head>
+  <body>
+    <h1>Products CRUD</h1>
+
+    <a href="create.php" class="btn btn-success">CREATE PRODUCT</a>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Price</th>
+      <th scope="col">Title</th>
+      <th scope="col">create_date</th>
+      <th scope="col">Action</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($Products as $i => $product): ?>
+        <tr>
+      <th scope="row"><?php echo $i + 1 ?></th>
+      <td></td>
+      
+      <td><?php echo $product['Price'] ?></td>
+      <td><?php echo $product['Title'] ?></td>
+      <td><?php echo $product['Create_Date'] ?></td>
+      <td>
+      <button type="button" class="btn btn-outline-primary">Edit</button>
+      <button type="button" class="btn btn-outline-danger">Delete</button>
+      </td>
+     
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+   
+  </body>
+</html>
